ba16d990cfed5b79fee9d4dd6859d7ed0d315f8b
Christian Martinez <cgmartinez11142@gmail.com>
2017-10-25 22:43:11 -0700

    add material light

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ import {
 const App = () => (
   <Router>
     <div>
-      <div>
-        <ul className="Header_links">
-          <li className="Header_link"><Link to="/">Home</Link></li>
-          <li className="Header_link"><Link to="/projects">Projects</Link></li>
-          <li className="Header_link"><Link to="/contact">Contact</Link></li>
-        </ul>
+      <div className="demo-layout-transparent mdl-layout mdl-js-layout">
+        <header className="mdl-layout__header mdl-layout__header--transparent">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">Christian Martinez</span>
+            <div className="mdl-layout-spacer"></div>
+            <ul className="mdl-navigation">
+              <li className="mdl-navigation__link"><Link to="/">Home</Link></li>
+              <li className="mdl-navigation__link"><Link to="/projects">Projects</Link></li>
+              <li className="mdl-navigation__link"><Link to="/contact">Contact</Link></li>
+            </ul>
+          </div>
+        </header>
       </div>
-      <Route exact path="/" component={Home}/>
-      <Route path="/projects" component={Projects}/>
-      <Route path="/contact" component={Contact}/>
+      <main className="mdl-layout__content">
+        <Route exact path="/" component={Home}/>
+        <Route path="/projects" component={Projects}/>
+        <Route path="/contact" component={Contact}/>
+      </main>
     </div>
   </Router>
 )
