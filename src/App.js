310e94caa9e75a1cee7a23923086ec848eac6393
Christian Martinez <cgmartinez11142@gmail.com>
2017-10-26 12:10:51 -0700

    add cards and styling

--- src/App.js
+++ src/App.js
@@ -10,33 +10,37 @@ const App = () => (
     <div className="demo-layout-transparent mdl-layout mdl-js-layout">
       <header className="mdl-layout__header mdl-layout__header--transparent">
         <div className="mdl-layout__header-row">
-          <span className="mdl-layout-title">Christian Martinez</span>
-          <div className="mdl-layout-spacer"></div>
-          <ul className="mdl-navigation">
-            <li className="mdl-navigation__link"><Link to="/">Home</Link></li>
-            <li className="mdl-navigation__link"><Link to="/projects">Projects</Link></li>
-            <li className="mdl-navigation__link"><Link to="/contact">Contact</Link></li>
-          </ul>
+          <nav className="mdl-navigation">
+            <a className="mdl-navigation__link"><Link to="/about">ABOUT</Link></a>
+            <a className="mdl-navigation__link is-active"><Link to="/work">WORK</Link></a>
+            <a className="mdl-navigation__link"><Link to="/contact">CONTACT</Link></a>
+          </nav>
         </div>
       </header>
       <main className="mdl-layout__content">
-        <Route exact path="/" component={Home}/>
-        <Route path="/projects" component={Projects}/>
-        <Route path="/contact" component={Contact}/>
+        <div className="mdl-grid content-max-width">
+          <Route exact path="/about" component={About}/>
+          <Route path="/work" component={Work}/>
+          <Route path="/contact" component={Contact}/>
+        </div>
       </main>
     </div>
   </Router>
 )
 
-const Home = () => (
+const About = () => (
   <div>
-    <h3>Fullstack Web Developer</h3>
+    <h3>Hello, I'm Christian Martinez, a Full Stack Web Developer</h3>
   </div>
 )
 
-const Projects = () => (
-  <div>
-    <h2>Projects</h2>
+const Work = () => (
+  <div className="mdl-cell mdl-card mdl-shadow--2dp">
+    <div className="mdl-card__title">
+      <h2 className="mdl-card__title-text">
+        Sample Project
+      </h2>
+    </div>
   </div>
 )
 
