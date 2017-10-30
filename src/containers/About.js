b41f47740da93c7e2f45a04aed6c7c13e16c0581
Christian Martinez <cgmartinez11142@gmail.com>
2017-10-30 16:22:26 -0700

    add cards to about and contact

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const About = () => (
-  <div>
-    <h3>Hello, I'm Christian Martinez, a Full Stack Web Developer</h3>
+  <div className="mdl-grid">
+    <div className="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
+      <h3>Hello, I'm Christian Martinez, a Full Stack Web Developer</h3>  
+    </div>
   </div>
 )
 
--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const Contact = () => (
-  <div>
-    <h3>You can reach me here:</h3>
+  <div className="mdl-grid">
+    <div className="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
+      <h3>You can reach me here:</h3>
+    </div>
   </div>
 )
 
