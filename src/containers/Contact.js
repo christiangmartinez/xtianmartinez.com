1d7fcccd926e78255694bdc31639e1d50da4a276
Christian Martinez <cgmartinez11142@gmail.com>
2017-10-31 15:00:04 -0700

    icons and spacing

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -8,6 +8,23 @@ const Contact = () => (
         <h2>Let's get in touch</h2>
       </div>
     </div>
+    <div className="mdl-cell mdl-cell--3-col"></div>
+      <div className="mdl-cell mdl-cell--2-col">
+        <img className="contact-button"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Contact_vmc2015.png/480px-Contact_vmc2015.png"
+          alt="email"/>
+      </div>
+      <div className="mdl-cell mdl-cell--2-col">
+      <img className="contact-button"
+        src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_black-512.png"
+        alt="linkedin"/>
+      </div>
+      <div className="mdl-cell mdl-cell--2-col">
+      <img className="contact-button"
+        src="https://www.shareicon.net/data/2015/09/25/107228_plus_512x512.png"
+        alt="linkedin"/>
+      </div>
+    <div className="mdl-cell mdl-cell--3-col"></div>
   </div>
 )
 
