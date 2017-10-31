4ddca84a86ba5ba585b091cb44a69a5dbf1d6323
Christian Martinez <cgmartinez11142@gmail.com>
2017-10-31 13:50:24 -0700

    adjust alignment of text over image cards

--- src/containers/About.js
+++ src/containers/About.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const About = () => (
   <div className="mdl-grid">
-    <div className="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
-      <h3>Hello, I'm Christian Martinez, a Full Stack Web Developer</h3>  
+    <div className="about-card mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
+      <img className="info-img" src="https://i.imgur.com/LmTU8KY.jpg" alt="chilling in the forest"/>
+      <div className="mdl-card__title">
+        <h2>Hello! I'm Christian Martinez.</h2>
+      </div>
+      <div className="mdl-card__supporting-text">
+        I'm a Fullstack Web Developer who is passionate about creating things, solving problems, and always finding something new to learn.
+      </div>
     </div>
   </div>
 )
