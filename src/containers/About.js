8d8acf0097f8f2b05800ccd8fa0c9087e319ba1e
Christian Martinez <cgmartinez11142@gmail.com>
2017-10-31 15:34:49 -0700

    alter gird on about.js

--- src/containers/About.js
+++ src/containers/About.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const About = () => (
   <div className="mdl-grid">
-    <div className="about-card mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
+    <div className="about-card mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
       <img className="info-img" src="https://i.imgur.com/LmTU8KY.jpg" alt="chilling in the forest"/>
       <div className="mdl-card__title">
         <h2>Hello! I'm Christian Martinez.</h2>
       </div>
     </div>
-    <div className="mdl-cell mdl-cell--3-col"></div>
-    <div className="mdl-cell mdl-cell--6-col">
+    <div className="mdl-cell mdl-cell--2-col"></div>
+    <div className="mdl-cell mdl-cell--8-col">
       <div className="about-text">
         I'm a Fullstack Web Developer with a passion for creating things, solving problems, and always finding something new to learn.
       </div>
     </div>
-    <div className="mdl-cell mdl-cell--3-col"></div>
+    <div className="mdl-cell mdl-cell--2-col"></div>
   </div>
 )
 
--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => (
   <div className="mdl-grid">
-    <div className="contact-card mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
+    <div className="contact-card mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
       <img className="info-img" src="https://www.voordewereldvanmorgen.nl/sites/default/files/styles/blog_image/public/mathyas-kurmann-102977.jpg?itok=JkWv-DqE" alt="mailboxes"/>
       <div className="mdl-card__title">
         <h2>Let's get in touch</h2>
