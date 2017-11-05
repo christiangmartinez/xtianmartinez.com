3416fd9f0ba32fd6f87097b119636a605e47fb23
Christian Martinez <cgmartinez11142@gmail.com>
2017-11-05 12:45:33 -0800

    finish line spacing on about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const About = () => (
-  <div className="mdl-grid">
-    <div className="greeting mdl-cell--12-col">
-      Hello there!<br/>
-      <span className="intro">My name is Christian.</span>
-    </div>
-    <div className="mdl-cell--12-col about-text">
-      I'm a Fullstack Web Developer with a passion for creating things, solving problems, and always finding something new to learn.
-    </div>
+<div className="mdl-grid">
+  <div className="greeting mdl-cell--12-col">
+    Hello there!<br/>
+    <span className="intro">My name is Christian.</span>
+    <br/>
+    <br/>
+    <p className="about-text">
+      I'm a Fullstack Web Developer with a passion for creating things,<br/>solving problems, and always finding something new to learn.
+    </p>
   </div>
+</div>
 )
 
 export default About
