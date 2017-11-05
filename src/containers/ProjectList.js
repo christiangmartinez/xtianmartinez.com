de82d03c98319501fb29f12ada808d56da33dd9b
Christian Martinez <cgmartinez11142@gmail.com>
2017-11-05 10:54:13 -0800

    change photo link

--- src/containers/ProjectList.js
+++ src/containers/ProjectList.js
@@ -3,19 +3,19 @@ import React from 'react'
 let projects = [{
   id: 0,
   url: 'project0',
-  coverImg:'https://www.japantimes.co.jp/wp-content/uploads/2014/08/p22-martin-art-of-princess-mononoke-a-20140803-870x499.jpg',
+  coverImg:'http://comicsalliance.com/files/2016/01/miyazaki-windrises1.jpg',
   title: 'Project 0',
   description: 'An Angular 2 web app to track Overwatch Characters.'
 },{
   id: 1,
   url: 'project1',
-  coverImg:'https://www.japantimes.co.jp/wp-content/uploads/2014/08/p22-martin-art-of-princess-mononoke-a-20140803-870x499.jpg',
+  coverImg:'https://i2.wp.com/www.geek-art.net/wp-content/uploads/2017/02/Guillaume-Morellec-Princess-Mononoke.jpg?resize=768%2C520',
   title: 'Project 1',
   description: 'A website that shows data on current near-earth objects.'
 },{
   id: 2,
   url: 'project2',
-  coverImg:'https://www.japantimes.co.jp/wp-content/uploads/2014/08/p22-martin-art-of-princess-mononoke-a-20140803-870x499.jpg',
+  coverImg:'https://cdn.sfstation.com/wp-content/uploads/2016/03/miyazaki_main.gif',
   title: 'Project 2',
   description: 'Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Non arcu risus quis varius quam quisque. Justo eget magna fermentum iaculis eu non diam.'
 }]
