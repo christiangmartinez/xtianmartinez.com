9391ef02cf958eb544d186766710ceac8ed11873
Christian Martinez <cgmartinez11142@gmail.com>
2017-11-06 13:32:31 -0800

    remove unneeded imports from app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-
 import Header from './components/Header'
 import Main from './components/Main'
 
