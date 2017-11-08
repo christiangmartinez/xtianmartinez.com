29bc8f7358c6bb26409e4b433d3533789b8f91cb
Christian Martinez <cgmartinez11142@gmail.com>
2017-11-07 22:25:53 -0800

    get some content on detail page

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -7,21 +7,29 @@ const projectList = {
     title: 'Flaming Space Rocks Tracker',
     type: 'Angular 2/Materialize',
     url: 'space_rocks',
-    description: 'proj'
+    description: "Flaming Space Rocks Tracker is an educational web app that teaches the user about asteroids and comets with and interactive timeline and display real-time info of asteroids passing earth using NASA's Near Earth Objects API. ",
+    featureImg0: 'https://i.imgur.com/rI2aG68.png',
+    featureDescription0: "Up-to-date data of objects currently    passing through earth's orbit with data specific to each  asteroid.",
+    featureImg1: 'https://i.imgur.com/gXE3maL.png',
+    featureDescription01: "Cards with asteroid info are color-coded. Passing objects that are potentially hazardous are dipslayed on a yellow card."
   },{
     id: 2,
     coverImg:'https://i.imgur.com/LA40dA5.png',
     title: 'Mappy',
     type: 'Native Android App',
     url: 'mappy',
-    description: 'proj'
+    description: "A location based social app. Get a feel for what's going on in your city with a quick glance at a map!",
+    featureImg0:'https://i.imgur.com/rI2aG68.png',
+    featureDescription0: 'Up-to-date data of objects currently passing through earths orbit with data specific to each asteroid.'
   },{
     id: 3,
     coverImg:'https://i.imgur.com/KbECryF.png',
     title: 'Overwatch Roster',
     type: 'Angular 2/Foundation',
     url: 'overwatch_roster',
-    description: 'proj'
+    description: 'proj',
+    featureImg0:'https://i.imgur.com/rI2aG68.png',
+    featureDescription0: 'Up-to-date data of objects currently passing through earths orbit with data specific to each asteroid.'
   }],
   all: function() { return this.projects},
   get: function(url) {
--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -9,9 +9,27 @@ const ProjectDetail = (props) =>  {
     return <div>ya done goofed</div>
   }
   return(
-    <div>
-      <h1>{project.title}</h1>
-    </div>
+    <div className="proj-detail mdl-grid">
+      <div className="mdl-cell--12">
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+      </div>
+      <div className="mdl-cell mdl-cell--6">
+        <img className="detail-image" src={project.featureImg0} alt="img"/>
+      </div>
+      <div className="mdl-cell mdl-cell--1"></div>
+      <div className="detail-feature mdl-cell mdl-cell--5">
+        <p>{project.featureDescription0}</p>
+      </div>
+      <div className="mdl-cell mdl-cell--12"></div>
+      <div className="detail-feature mdl-cell mdl-cell--5">
+        <p>{project.featureDescription01}</p>
+      </div>
+      <div className="mdl-cell mdl-cell--1"></div>
+      <div className="mdl-cell mdl-cell--6">
+        <img className="detail-image" src={project.featureImg1} alt="img"/>
+      </div>
+  </div>
   )
 }
 export default ProjectDetail
