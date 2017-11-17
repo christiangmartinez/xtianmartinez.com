4e9abac1de23dce38f68d0b1722382680f7d5175
Christian Martinez <cgmartinez11142@gmail.com>
2017-11-16 21:24:08 -0800

    get conditionl rendering of buttons

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -14,7 +14,8 @@ const projectList = {
     featureDescription1: "Cards with asteroid info are color-coded. Passing objects that are potentially hazardous are dipslayed on a yellow card.",
     featureImg2: 'https://i.imgur.com/Oyt8G5x.png',
     featureDescription2: 'An interactive timeline teaches the user about the history of comets and asteroids.',
-    website: 'https://asteroid-data.firebaseapp.com/'
+    website: 'https://asteroid-data.firebaseapp.com/',
+    github: ''
   },{
     id: 2,
     coverImg:'https://i.imgur.com/Eoce0t8.jpg',
@@ -28,7 +29,8 @@ const projectList = {
     featureDescription1: "Create user accounts and secure login with Firebase.",
     featureImg2: 'https://i.imgur.com/3rdcLHC.png',
     featureDescription2: 'Comments, latitude, and longitude, are saved in Firebase.',
-    website: 'https://github.com/christiangmartinez/chismaps'
+    website: null,
+    github: 'https://github.com/christiangmartinez/chismaps'
   },{
     id: 3,
     coverImg:'https://i.imgur.com/KbECryF.png',
@@ -42,7 +44,8 @@ const projectList = {
     featureDescription1: "Dynamically routed detail pages for each character.",
     featureImg2: 'https://i.imgur.com/ai5d1SJ.png',
     featureDescription2: 'Characters can be added, updated, and deleted with changes reflected in real-time in Firebase.',
-    website: 'https://github.com/christiangmartinez/overwatch-roster'
+    website: null,
+    github: 'https://github.com/christiangmartinez/overwatch-roster'
   }],
   all: function() { return this.projects},
   get: function(url) {
